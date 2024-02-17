5c18abecd055a2a7afed2043cb24cb6333070f97
kan-software <krystianantkowiak001@gmail.com>
2024-02-17 17:40:53 +0100

    Setup tools

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,21 @@
 module.exports = {
   root: true,
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:testing-library/react',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -15,4 +26,4 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
-}
+};
